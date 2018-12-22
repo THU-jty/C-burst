5cca6803b36cd120df439ede231f95eef234b03a
prince-de-marcia <jty1313@126.com>
2018-12-22 20:54:07 +0800

    12.22

--- main.cpp
+++ main.cpp
@@ -20,15 +20,15 @@ public:
     ll id;
     block( ll t, ll i ): time(t), id(i){}
     bool operator <( const block &o ) const{
-        if( time == o.time ) return id < o.id;
-        else return time < o.time;
+        if( id == o.id ) return time < o.time;
+        else return id < o.id;
     }
 };
 
 class LRU
 {
 public:
-    int m, n, cnt;// m ʵ�ʴ�С n Ŀǰ�Ĵ�С
+    int m, n, cnt;// m 实际大小 n 目前的大小
     ll *tag;
     ll *stp;
     set< pair<ll,ll> >tt;
@@ -90,11 +90,17 @@ public:
 class BG
 {
 public:
-    set<block>q;
+    set<block>*q;
     ll ep;
     bool operator <( const BG &o )const{
         return ep < o.ep;
     }
+    BG(){
+        q = new set<block>;
+    }
+//    ~BG(){
+//        delete q;
+//    }
 };
 
 class EAR
@@ -107,31 +113,36 @@ public:
         tot = 0;
         up = area/2;
     }
-    bool find( block a ){
-        set<block>::iterator it;
-        it = st.find( block(-1, a.id) );
-        if( it == st.end() || it->id != a.id ) return 1;
-        return 0;
-    }
-    void ins_bg( BG bg ){
+    void ins_bg( BG &bg ){
         int i = 29;
-        while( tot+bg.q.size() > up ){
+        while( tot+bg.q->size() > up ){
             if( p[i].size() != 0 ){
+               //printf("%d size %d\n", i, p[i].size());
                 set<BG>::iterator is = p[i].begin();
-                tot -= is->q.size();
+               // printf("ep %lld size %d\n", is->ep, is->q->size());
+                tot -= is->q->size();
                 set<block>::iterator it;
-                for( it = is->q.begin(); it != is->q.end(); it ++ ){
+                for( it = is->q->begin(); it != is->q->end(); it ++ ){
+//                    if( it->id == 1712566 && it->time == 464095000 ) printf("out %lld\n", tot);
                     st.erase( block( it->time, it->id ) );
                 }
+               // printf("BG out %lld tot %lld\n", is->ep, tot);
                 p[i].erase( is );
-                i --;
-                if( i < 0 ) i += 30;
             }
+            i --;
+            if( i < 0 ) i += 30;
         }
-        ll tmp = log( bg.q.size()+0.5 );
+        ll tmp = log( bg.q->size()+0.5 );
         p[tmp].insert( bg );
+//        printf("tmp %lld\n", tmp);
         set<block>::iterator it;
-        st.insert( block( it->time, it->id ) );
+        for( it = bg.q->begin(); it != bg.q->end(); it ++ ){
+//            if( it->id == 1712566 && it->time == 464095000 )
+//                printf("set in %lld\n", tot);
+            st.insert( block( it->time, it->id ) );
+        }
+        tot += bg.q->size();
+//        printf("BG in %lld\n", bg.ep);
     }
     void dec(){
         int i = 29;
@@ -139,55 +150,68 @@ public:
         while( tot > up ){
             if( p[i].size() != 0 ){
                 set<BG>::iterator is = p[i].begin();
-                tot -= is->q.size();
+                tot -= is->q->size();
                 set<block>::iterator it;
-                for( it = is->q.begin(); it != is->q.end(); it ++ ){
+                for( it = is->q->begin(); it != is->q->end(); it ++ ){
+                    //if( it->id == 1712566 && it->time == 464095000 ) printf("out %lld\n", tot);
                     st.erase( block( it->time, it->id ) );
                 }
                 p[i].erase( is );
-                i --;
-                if( i < 0 ) i += 30;
             }
+            i --;
+            if( i < 0 ) i += 30;
         }
+    }
+    bool find( block a ){
+        set<block>::iterator it;
+        it = st.lower_bound( block(-1, a.id) );
+        if( it == st.end() || it->id != a.id ) return 1;
+        return 0;
     }
     void del( block a ){
         int ii, jj, i, j;
         tot --;
         ii = jj = -1;
         set<block>::iterator it;
-        it = st.find( block(-1, a.id) );
-        if( it == st.end() || it->id != a.id ){ puts("error"); exit(1); }
+        it = st.lower_bound( block(-1, a.id) );
+        if( it == st.end() || it->id != a.id ){ puts("error1"); exit(1); }
         ll time = it->time;
         st.erase(it);
         BG tmp;
+//        printf("query ep %lld\n", time/epoch);
         for( i = 0; i < 30; i ++ ){
             set<BG>::iterator is;
             BG bg;
-            bg.ep = it->time/epoch;
+            bg.ep = time/epoch;
+            //bg.q = NULL;
             is = p[i].lower_bound( bg );
             if( is == p[i].end() || is->ep != bg.ep ) continue;
+            //if( is == p[i].end() ) continue;
+            //else if( is->ep != it->time/epoch ) continue;
             else{
                 set<block>::iterator ir;
-                ir = is->q.find( a );
-                if( ir == is->q.end() || ir->time != a.time || ir->id != a.id ){ puts("error"); exit(1); }
-                (is->q).erase( );
+                ir = is->q->find( block(time, a.id) );
+                if( ir == is->q->end() ){ puts("error2"); exit(1); }
+                is->q->erase( ir );
 
-                if( is->q.size() < pow( 2.0, ii ) ){
+                if( is->q->size() < pow( 2.0, ii ) ){
                     BG tmp = *is;
                     p[i].erase(is);
-                    if( tmp.q.size() != 0 ){
+                    if( tmp.q->size() != 0 ){
                         p[i-1].insert( tmp );
                     }
                 }
                 break;
             }
         }
-        if( i == 30 ){ puts("error"); exit(1); }
+//        if( i == 30 ){
+//            printf("time %lld id %lld\n", time, a.id);
+//            puts("error3"); exit(1);
+//        }
     }
     void ins( block a ){
         int ii, jj, i, j;
         tot ++;
-        st.insert( a );
         for( i = 0; i < 30; i ++ ){
             set<BG>::iterator is;
             BG bg;
@@ -196,12 +220,13 @@ public:
             if( is == p[i].end() || is->ep != bg.ep ) continue;
             else{
                 //set<block>::iterator ir;
-                //is->q.insert( a );
-                if( is->q.size() >= pow( 2.0, ii+1 ) ){
+                is->q->insert( a );
+                if( is->q->size() >= pow( 2.0, ii+1 ) ){
                     BG tmp = *is;
                     p[i].erase( is );
                     p[i+1].insert(tmp);
                 }
+                st.insert( a );
                 break;
             }
         }
@@ -358,6 +383,7 @@ int main()
         }
         time -= base;
         ++ tot;
+        if( tot%1000 == 0 ) printf("%lld ok\n", tot);
 
         int ft = 0;
         for( ll x = offset/bz; x < (offset+siz+bz-1)/bz; x ++ ){
@@ -375,18 +401,23 @@ int main()
             fprintf(c1, "%lld %lld %lld\n", time, offset, siz);
         }
         else hit1 ++;
-
-        if( time-pre >= epoch ){
+        if( time/epoch != pre/epoch ){
+            //printf("pre %lld\n", pre+128166372003061000);
             bg.ep = pre/epoch;
-            for( i = 0; i < bff.n; i ++ ){
-                if( bff.tag[i] != -1 ){
-                    bg.q.insert( block( bff.stp[i], bff.tag[i] ) );
-                }
+
+            set< pair<ll,ll> >::iterator it;
+            for( it = bff.tt.begin(); it != bff.tt.end(); it ++ ){
+                ll x = it->first;
+                ll y = bff.tag[ it->second ];
+                bg.q->insert( block( x, y ) );
+                //if( x == 464095000 && y == 1712566 ) printf("in %lld\n", tot);
             }
+
             ear.ins_bg( bg );
             pre = time;
             //printf("%lld epoch insert\n", tot);
         }
+        //else printf("%lld\n", tot);
 
         int fl = 0;
         for( ll x = offset/bz; x < (offset+siz+bz-1)/bz; x ++ ){
